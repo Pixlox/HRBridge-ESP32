0c99cd62feb3da14cd8233749f517ee1767ee211
Pixlox <85137818+Pixlox@users.noreply.github.com>
2026-01-27 12:15:22 +1100

    iniital

---
+++ HRBridge/HRBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HRBridgeApp.swift
+//  HRBridge
+//
+//  Created by Omar Hafeezullah on 27/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct HRBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HRBridgeTests/HRBridgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  HRBridgeTests.swift
+//  HRBridgeTests
+//
+//  Created by Omar Hafeezullah on 27/1/2026.
+//
+
+import Testing
+@testable import HRBridge
+
+struct HRBridgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
